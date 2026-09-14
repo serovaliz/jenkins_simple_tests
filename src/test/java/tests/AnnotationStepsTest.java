cf45f24fd061c9080fa665b0ff65b0b91de8f6c2
Elizaveta Serova <serovaliz@gmail.com>
2026-09-14 17:38:14 +0300

    Added test with Annotation steps Added test just with addListener Added test with Lambda Steps

--- src/test/java/tests/AnnotationStepsTest.java
+++ src/test/java/tests/AnnotationStepsTest.java
@@ -1,16 +1,24 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
-public class IssueTestsWithAnnotation {
+public class AnnotationStepsTest {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.browserSize = "1920x1080";
+
+    }
 
     @Test
-    @DisplayName("Тест с использованием степов с аннотацией")
-    public void testGithubIssue() {
+    @DisplayName("Тест с использованием степов с аннотацией @Step")
+     public void testGithubIssue() {
         SelenideLogger.addListener("allure", new AllureSelenide());
         WebSteps webSteps = new WebSteps();
 
