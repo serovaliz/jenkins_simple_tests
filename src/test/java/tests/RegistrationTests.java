dfda785c3552228912f15c934558259c22e6c494
Elizaveta Serova <serovaliz@gmail.com>
2026-09-15 17:31:59 +0300

    Added UI Tests

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -3,7 +3,12 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+
+import java.util.List;
+import java.util.Map;
 
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
@@ -14,8 +19,23 @@ public class RegistrationTests {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = "chrome";
+//        Configuration.browserVersion = "144.0";
         Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+//        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 10000; // default 4000
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments(List.of("--disable-dev-shm-usage", "--no-sandbox"));
+        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @Test
@@ -35,7 +55,7 @@ public class RegistrationTests {
         $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
         $("#subjectsInput").setValue("Math").pressEnter();
         executeJavaScript("arguments[0].click();", $("#hobbiesWrapper").$(byText("Sports")));
-        $("#uploadPicture").uploadFromClasspath("niL25B_U0dp0XYfPL9PGX_KW2owUdihzD49355BCH-uiAJCTPXZfebRgwHn7K8Z4CvQBT_D6dweCmROZTZAJqGwu.jpg");
+        $("#uploadPicture").uploadFromClasspath("img/niL25B_U0dp0XYfPL9PGX_KW2owUdihzD49355BCH-uiAJCTPXZfebRgwHn7K8Z4CvQBT_D6dweCmROZTZAJqGwu.jpg");
         $("#currentAddress").setValue("Some address 1");
         $("#state").scrollIntoView(true);
         $("#react-select-3-input").setValue("NCR").pressEnter();
